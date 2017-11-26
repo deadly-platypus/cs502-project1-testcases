9b7d7cf0d2e3dc118e59e125ac383a4508c3eb26
mckee15 <mckee15@purdue.edu>
2017-11-26 14:43:02 -0500

    improving iteratetests

--- iteratetests.sh
+++ iteratetests.sh
@@ -1,11 +1,17 @@
 rm report.txt
 
+if [[ $# != 1 ]]; then
+	echo "Please provide a compiler"
+	exit 1
+fi
+
 CC=$1
 
 for f in *.c; do
     testname="${f%.*}"
     testnum=${testname:4}
-    $CC $f
+    echo "Running test $testnum"
+    $CC $f &> /dev/null
     DIFF=`diff t$testnum.txt output.txt`
     if [[ $DIFF != "" ]]; then
         echo "Test $testnum failed:" >> report.txt
